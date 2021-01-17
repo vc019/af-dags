e6a7115a51f73f82e4ee6c83fea1caec8357120a
vc019 <vc019>
2021-01-17 20:03:25 +0000

    Test Connection

---
+++ test_conn_env.py
@@ -0,0 +1,32 @@
+# Filename: test_conn_env.py
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+  'owner': 'airflow',
+  'depends_on_past': False,
+  'start_date': datetime(2020, 12, 30),
+  'email': ['vipin.chadha@gmail.com'],
+  'email_on_failure': False,
+  'email_on_retry': False,
+  'retries': 1,
+  'retry_delay': timedelta(minutes=5),
+  'catchup': False
+}
+
+dag = DAG('test_conn', schedule_interval='0 0 1 * *' ,
+  default_args=default_args)
+create_command = 'echo   $AIRFLOW_CONN_POSTGRES_MASTER'
+t1 = BashOperator(
+  task_id='print_date',
+  bash_command='date',
+  dag=dag
+)
+
+t2 = BashOperator(
+  task_id= 'myBashTest',
+  bash_command=create_command,
+  dag=dag
+)
+t2.set_upstream(t1)
