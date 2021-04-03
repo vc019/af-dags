71a2deff72da086b1895d02bbe262509ed9156a9
vc019 <vc019>
2021-04-03 12:05:00 +0100

    Refactored all dags to align with demo

--- flowgi_demo1.py
+++ flowgi_demo1.py
@@ -4,35 +4,35 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
 default_args = {
-  'owner': 'airflow',
-  'depends_on_past': False,
-  'start_date': datetime(2020, 12, 30),
-  'email': ['vipin@flowg.io'],
-  'email_on_failure': False,
-  'email_on_retry': False,
-  'retries': 1,
-  'retry_delay': timedelta(minutes=5),
-# 'queue': 'bash_queue',
-# 'pool': 'backfill',
-# 'priority_weight': 10,
-# 'end_date': datetime(2016, 1, 1),
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 12, 30),
+    'email': ['vipin@flowg.io'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
 }
 
 dag = DAG('flowgi_demo_1',
-          schedule_interval='0 0 1 * *' ,
+          schedule_interval='0 0 1 * *',
           catchup=False,
           default_args=default_args)
 
 create_command = 'echo   HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO '
 t1 = BashOperator(
-  task_id='print_date',
-  bash_command='date',
-  dag=dag
+    task_id='print_date',
+    bash_command='date',
+    dag=dag
 )
 
 t2 = BashOperator(
-  task_id= 'myBashTest',
-  bash_command=create_command,
-  dag=dag
+    task_id='myBashTest',
+    bash_command=create_command,
+    dag=dag
 )
 t2.set_upstream(t1)
